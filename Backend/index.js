ac5e4dd3e76917fc53ff624f58b60f134be1a2dc
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-11-25 18:05:42 +0600

    Schema created

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const chats = require("./Data/data");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 dotenv.config();
 
