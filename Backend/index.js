8c0dabbb61177ccca8c0422a90cb9ae1e781926d
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-11-25 09:03:55 +0600

    Server is ready

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const chats = require("./Data/data");
+const dotenv = require("dotenv");
 
 const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 
 app.get("/", async (req, res) => {
   res.send("Server is running");
 });
 
-app.listen(5000, console.log("Hello"));
+app.get("/api/chat", async (req, res) => {
+  res.send(chats);
+});
+
+app.listen(port, console.log("Hello"));
